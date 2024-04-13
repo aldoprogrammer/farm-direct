dab9869cf1e5f7cf96a0e96e6c52a16e81d5dc50
aldoprogrammer <aldobesma@gmail.com>
2024-04-13 10:55:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Accordion, AccordionBody, AccordionHeader, Button } from '@material-tailwind/react'
+
+function App() {
+
+ 
+  return (
+    <>
+      <Button color="green">Material</Button>
+    </>
+  )
+}
+
+export default App
