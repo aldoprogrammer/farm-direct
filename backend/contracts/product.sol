c88f3fdf2c6e833e84564738340fe7604cff56cd
stephenmaina2995 <stephenmaina2995@gmail.com>
2024-04-20 19:54:51 +0300

    Added ERC20 contacts

--- backend/contracts/product.sol
+++ backend/contracts/product.sol
@@ -1,13 +1,27 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.4.0 <0.9.0;
+
+// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Product {
+    // constructor() ERC20 ("Product", "FmD"){}
     uint32 public product_id = 0;
 
-        struct product{
-         uint product_id;
+        struct productInfo{
+         uint256 product_id;
+         string productName;
+         uint256 merchant_id;
+        }
+        struct orders {
+            uint256 order_id;
+            uint256 user_id;
         }
 
-    mapping(uint32=> product) public newProduct;
+    mapping(uint32=> productInfo) public newProduct;
+    // Added two functions for creating (CREATE) and getting (READ) (CRUD)
+    function addProduct(uint256 _productInfo) public {
+        string memory message = "FarmDirect";
+        return (message);
+    }
 }
--- backend/contracts/user.sol
+++ backend/contracts/user.sol
@@ -29,11 +29,11 @@ contract MyUser{
     
 
 //     // Create user
-    // function addPerson () public {
+    function addPerson () public {
         // person[msg.sender].push(person(person));
         // return personId;
-    // }
-//     // Read user
+    }
+    // Read user
 //     modifier onlyUser() {
 //         require(msg.sender==user);
 //         _;
@@ -41,54 +41,7 @@ contract MyUser{
     function getPerson (uint32 _personId) public {
         // return personId;
     }
-//     // Update user
-//     function editUser () public{
-//         return User;
-//     }
-//     // Delete user
-//     function deleteUser () public{
-//         return User;
-//     }
-// }
 
-// contract MyProduct is MyUser {
-//     struct Product{
-//         uint id;
-//         string text;
-//         bool completed;
-//     }
 
-//     // Create product
-//     function addProduct() public {
-//         for (numProducts>=0; numProducts; numProducts++){
-//             if(numProd>=0){
-//                 return products;
-//             }
-//         }
-//     }
-//     // Read
-    function getProduct(uint32 _productId) public {
-//         for (numProducts>=0;numProducts;numProducts++){
-//             if(numProd>=0){
-//                 return products;
-//             }
-//         }
-        
-    }
-//     // Update
-//     function editProduct() public {
-//         for (numProducts>=0;numProducts;numProducts++){
-//             if(numProd>=0){
-//                 return products;
-//             }
-//         }
-//     }
-//     // Delete
-//     function deleteProduct() public {
-//         for (numProducts>=0;numProducts;numProducts++){
-//             if(numProd>=0){
-//                 return products;
-//             }
-//         }
-//     }
+
 }
