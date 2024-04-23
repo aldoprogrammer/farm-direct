bdc04efbf7ab0d185a2eb34785c6356ca7bd5ab1
aldoprogrammer <aldobesma@gmail.com>
2024-04-23 15:35:46 +0700

    update

--- src/pages/product-page/EditPage.jsx
+++ src/pages/product-page/EditPage.jsx
@@ -54,14 +54,14 @@ const EditPage = ({ setActiveProductTab }) => {
         flex items-center justify-center rounded-md cursor-pointer'>
           <p className='text-base text-[#F73B3B]'>Deactive</p>
         </div>
-        <div className='p-2 border-[#D8D8DA] border-2
+        <div className='p-2 border-[#65A30D] border-2
         flex items-center justify-center rounded-md'>
-          <p className='text-base text-[#D8D8DA] cursor-pointer'>Copy</p>
+          <p className='text-base text-[#202020] cursor-pointer'>Copy</p>
         </div>
-        <div className='p-2 border-[#D8D8DA] border-2
+        <div className='p-2 border-[#65A30D] border-2
         flex items-center justify-center rounded-md cursor-pointer'
         onClick={handlePreviewMarketTab}>
-          <p className='text-base text-[#D8D8DA] text-center'>Preview In Market</p>
+          <p className='text-base text-[#202020] text-center'>Preview In Market</p>
         </div>
       </div>
       {/* thubmnail product section */}
