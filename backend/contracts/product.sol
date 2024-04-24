61aecd05aa0242ea94a85395f9a4b7290efa854d
stephenmaina2995 <stephenmaina2995@gmail.com>
2024-04-24 10:22:22 +0300

    Updated the create products function

--- backend/contracts/product.sol
+++ backend/contracts/product.sol
@@ -9,7 +9,7 @@ contract Product is farm {
     
     uint32 public product_id = 0;
 
-        struct productInfo{
+        struct productDetails{
          uint256 product_id;
          string productName;
          uint256 merchant_id;
@@ -22,7 +22,7 @@ contract Product is farm {
             uint256 countrycode;
         }
 
-    mapping(uint32=> productInfo) public newProduct;
+    mapping(uint32=> productDetails) public products;
     mapping(address => uint256) balances;
     // Added two functions for creating (CREATE) and getting (READ) (CRUD)
     // paying ether for products
@@ -41,8 +41,9 @@ contract Product is farm {
     function sendingEther(address payable farmer) external{
        farmer.transfer(1 ether); 
     }
-    function addProduct(uint256 _product_id, string memory _productName, uint256 _merchant_id, uint256 _timestamp) public {
-        
+    function addProduct(uint256 _product_id, string memory _productName, uint256 _merchant_id, uint256 _timestamp) public returns (string memory){        
+        ProductDetails memory newProduct=ProductDetails(uint32(_product_id))
+        products[_product_id] = newProduct;
         return "New Product";
     }
     function getProduct(uint256 _productInfo) public {
