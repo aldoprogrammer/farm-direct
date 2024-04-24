a6f700ad27829b5a78da21be129a8b6e977f387a
aldoprogrammer <aldobesma@gmail.com>
2024-04-24 21:17:31 +0700

    update

--- src/pages/market/HomeMarket.jsx
+++ src/pages/market/HomeMarket.jsx
@@ -62,38 +62,38 @@ const HomeMarket = ({ setActiveMarketTab }) => {
 
   return (
     <div className='flex flex-col w-full h-[600px] px-2 py-0 items-center mt-4 gap-4 overflow-auto mb-20'>
-        <h2 className='text-[#000000] text-xl font-roboto-slab 
+      <h2 className='text-[#000000] text-xl font-roboto-slab 
          font-black '>
-          Welcome to our Market Place!
-          </h2>
+        Welcome to our Market Place!
+      </h2>
       {/* product section */}
       <div className='w-full gap-5 grid grid-cols-1
        md:grid-cols-2 lg:grid-cols-3 mb-4'>
-      {dataDummyMarket.map((item, index) => (
-        <div key={index}
-          className='w-full rounded-xl 
+        {dataDummyMarket.map((item, index) => (
+          <div key={index}
+            className='w-full rounded-xl 
            text-[#000000] h-[153px] flex flex-row 
-           shadow-md p-4 gap-2'
-          style={{ backgroundImage: `url(${item.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-onClick={handleSeekingProducts}
+           shadow-md p-4 gap-2 cursor-pointer'
+            style={{ backgroundImage: `url(${item.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+            onClick={handleSeekingProducts}
           >
-          <div className='flex flex-col gap-1 w-3/5 mt-2'>
-            <h5 className='font-bold text-white 
+            <div className='flex flex-col gap-1 w-3/5 mt-2'>
+              <h5 className='font-bold text-white 
             md:text-2xl text-xl
             cursor-pointer'
-            onClick={handleDetailsMarketProduct}>
-              {item.farmerName}
-            </h5>
-            <Rating value={3} readonly />
-            <button className='w-fit bg-[#65A30D] py-[7px] px-4 rounded-md text-[#FFFFFF] my-2 flex flex-row items-center gap-3'
-                         
-            >
-              {/* <FontAwesomeIcon icon={faCartShopping} className='text-white w-4 h-4' /> */}
-              <p>Visit</p>
-            </button>
+                onClick={handleDetailsMarketProduct}>
+                {item.farmerName}
+              </h5>
+              <Rating value={3} readonly />
+              <button className='w-fit bg-[#65A30D] py-[7px] px-4 rounded-md text-[#FFFFFF] my-2 flex flex-row items-center gap-3'
+
+              >
+                {/* <FontAwesomeIcon icon={faCartShopping} className='text-white w-4 h-4' /> */}
+                <p>Visit</p>
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
       {showModalPurchase && <PurchaseModal closeModal={handleCloseModalPurchase} />}
       {showModalAddress && <FarmAddressDetailsFormModal closeModal={handleCloseEditAddress} />}
